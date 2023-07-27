1372b0bb3f852c1a4fb1ff86ebd752c72dde1e9e
Van Den Biggelaar <matthieu.van.den.biggelaar@ing.com>
2023-07-27 11:52:43 +0200

    chore: remove uneeded comments

--- src/js/game.js
+++ src/js/game.js
@@ -3,19 +3,16 @@ import { DefaultScene } from "./scenes/DefaultScene.js";
 import { Scene1 } from "./scenes/Scene1.js";
 import { Scene2 } from "./scenes/Scene2.js";
 
-// Configuration for the game
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
   parent: "game-container",
-  scene: [DefaultScene, Scene1, Scene2], // Add all scene classes to the scene configuration
+  scene: [DefaultScene, Scene1, Scene2],
 };
 
-// Create the game instance
 const game = new Phaser.Game(config);
 
-// Function to resize the game to fit the screen proportionally
 function resizeGame() {
   const canvas = document.querySelector("canvas");
   const windowWidth = window.innerWidth;
@@ -32,8 +29,6 @@ function resizeGame() {
   }
 }
 
-// Call the resize function initially to fit the game on the screen
 resizeGame();
 
-// Add an event listener for the window resize event
 window.addEventListener("resize", resizeGame);
