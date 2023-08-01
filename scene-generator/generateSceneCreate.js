1d08abf59b85eb1ce51e365eb66214fd5dc8b375
Matthieu van den Biggelaar <vdbiggelaar@gmail.com>
2023-08-01 03:57:04 +0200

    feat: add sceneTransition action on collide

--- scene-generator/generateSceneCreate.js
+++ scene-generator/generateSceneCreate.js
@@ -11,135 +11,6 @@ function generateSceneCreate(sceneName, sceneConfig) {
     createCode += `this.cameras.main.setBackgroundColor(${background?.color});\n`;
   }
 
-  actionableItems.forEach((actionableItem) => {
-    if (actionableItem?.type === 'hitbox') {
-      createCode += `this.${actionableItem?.name} = this.add.rectangle(${actionableItem?.position?.x}, ${actionableItem?.position?.y}, ${actionableItem?.size?.width}, ${actionableItem?.size?.height}, ${actionableItem?.backgroundColor}).setInteractive();\n`;
-    }
-
-    if (actionableItem?.type === 'text') {
-      createCode += `this.${actionableItem?.name} = this.add.text(${
-        actionableItem?.position?.x
-      }, ${actionableItem?.position?.y}, "${
-        actionableItem?.text?.content
-      }", ${JSON.stringify(actionableItem?.text?.styles)}).setOrigin(${
-        actionableItem?.text?.origin || 0.5
-      }).setScale(${actionableItem?.text?.scale || 1})\n`;
-    }
-
-    if (actionableItem?.type === 'image') {
-      createCode += `this.${actionableItem?.name} = this.add.image(${actionableItem?.position?.x}, ${actionableItem?.position?.y}, "${actionableItem?.name}").setInteractive();\n`;
-      if (actionableItem?.image && actionableItem?.image?.scale) {
-        createCode += `this.${actionableItem?.name}.setScale(${actionableItem?.image?.scale});\n`;
-      }
-    }
-
-    if (actionableItem?.animation) {
-      createCode += `
-        this.tweens.add({
-          targets: this.${actionableItem?.name},
-          ${Object.entries(actionableItem?.animation?.options)
-            .map(
-              ([key, value]) =>
-                `${key}: ${typeof value === 'string' ? `'${value}'` : value}`
-            )
-            .join(',')}
-        });
-      `;
-    }
-
-    if (actionableItem?.isDraggable) {
-      createCode += `
-        this.input.setDraggable(this.${actionableItem?.name});
-        this.${actionableItem?.name}.on('pointerdown', function () {
-          this.scene.children.bringToTop(this);
-        });
-
-        this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
-          gameObject.x = dragX;
-          gameObject.y = dragY;
-        });
-      `;
-    }
-
-    if (actionableItem?.hasPhysicsEnabled) {
-      createCode += `this.physics.world.enable(this.${actionableItem?.name});`;
-    }
-
-    function generateMainNPCDialogAction(action) {
-      return `
-        this.mainNPC.dialogContent = "${action.dialog?.content}";
-        this.mainNPC.showDialog(this.mainNPC.dialogContent, ${
-          action.dialog?.duration || 3000
-        });
-      `;
-    }
-
-    function generateActions(name, eventType, eventTarget, actions) {
-      let content = '';
-      if (eventType === 'collide') {
-        actions.forEach(({ actionType, action }) => {
-          content += `
-            this.physics.add.overlap(this.${name}, this.${eventTarget}, () => {
-              ${
-                actionType === 'mainNPCDialog'
-                  ? generateMainNPCDialogAction(action)
-                  : ''
-              }
-            });
-          `;
-        });
-      } else {
-        actions.forEach(({ actionType, action }) => {
-          content += `
-            this.${name}.on("${eventType.toLowerCase()}", function () {
-              ${
-                actionType === 'sceneTransition'
-                  ? `
-                if (!isTransitionInProgress) {
-                  isTransitionInProgress = true;
-                  this.cameras.main.${action?.transition?.effect}(${action?.transition?.options}, (camera, progress) => {
-                    if (progress === 1) {
-                      isTransitionInProgress = false;
-                      this.scene.start("${action?.transition?.to}");
-                    }
-                  });
-                }
-                `
-                  : ''
-              }
-              ${
-                actionType === 'mainNPCDialog'
-                  ? generateMainNPCDialogAction(action)
-                  : ''
-              }
-            }, this);
-          `;
-        });
-      }
-      return content;
-    }
-
-    if (actionableItem?.label) {
-      createCode += `
-        this.add.text(
-          ${actionableItem.position.x},
-          ${actionableItem.position.y - actionableItem.size.height},
-          "${actionableItem.label}",
-          ${JSON.stringify(sceneConfig?.labelStyles)}
-        ).setOrigin(0.5);
-      `;
-    }
-
-    actionableItem?.events.forEach(({ eventType, actions, eventTarget }) => {
-      createCode += generateActions(
-        actionableItem?.name,
-        eventType,
-        eventTarget,
-        actions
-      );
-    });
-  });
-
   if (mainNPC) {
     createCode += `
       this.mainNPC.create();
@@ -179,6 +50,148 @@ function generateSceneCreate(sceneName, sceneConfig) {
     `;
   }
 
+  if (actionableItems && actionableItems.length > 0) {
+    actionableItems?.forEach((actionableItem) => {
+      if (actionableItem?.type === 'hitbox') {
+        createCode += `this.${actionableItem?.name} = this.add.rectangle(${actionableItem?.position?.x}, ${actionableItem?.position?.y}, ${actionableItem?.size?.width}, ${actionableItem?.size?.height}, ${actionableItem?.backgroundColor}).setInteractive();\n`;
+      }
+
+      if (actionableItem?.type === 'text') {
+        createCode += `this.${actionableItem?.name} = this.add.text(${
+          actionableItem?.position?.x
+        }, ${actionableItem?.position?.y}, "${
+          actionableItem?.text?.content
+        }", ${JSON.stringify(actionableItem?.text?.styles)}).setOrigin(${
+          actionableItem?.text?.origin || 0.5
+        }).setScale(${actionableItem?.text?.scale || 1})\n`;
+      }
+
+      if (actionableItem?.type === 'image') {
+        createCode += `this.${actionableItem?.name} = this.add.image(${actionableItem?.position?.x}, ${actionableItem?.position?.y}, "${actionableItem?.name}").setInteractive();\n`;
+        if (actionableItem?.image && actionableItem?.image?.scale) {
+          createCode += `this.${actionableItem?.name}.setScale(${actionableItem?.image?.scale});\n`;
+        }
+      }
+
+      if (actionableItem?.animation) {
+        createCode += `
+          this.tweens.add({
+            targets: this.${actionableItem?.name},
+            ${Object.entries(actionableItem?.animation?.options)
+              .map(
+                ([key, value]) =>
+                  `${key}: ${typeof value === 'string' ? `'${value}'` : value}`
+              )
+              .join(',')}
+          });
+        `;
+      }
+
+      if (actionableItem?.isDraggable) {
+        createCode += `
+          this.input.setDraggable(this.${actionableItem?.name});
+          this.${actionableItem?.name}.on('pointerdown', function () {
+            this.scene.children.bringToTop(this);
+          });
+
+          this.input.on("drag", (pointer, gameObject, dragX, dragY) => {
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+          });
+        `;
+      }
+
+      if (actionableItem?.hasPhysicsEnabled) {
+        createCode += `this.physics.world.enable(this.${actionableItem?.name});`;
+      }
+
+      function generateMainNPCDialogAction(action) {
+        return `
+          this.mainNPC.dialogContent = "${action.dialog?.content}";
+          this.mainNPC.showDialog(this.mainNPC.dialogContent, ${
+            action.dialog?.duration || 3000
+          });
+        `;
+      }
+
+      function generateSceneTransition(action) {
+        return `
+        if (!isTransitionInProgress) {
+          isTransitionInProgress = true;
+          this.cameras.main.${action?.transition?.effect}(${action?.transition?.options}, (camera, progress) => {
+            if (progress === 1) {
+              isTransitionInProgress = false;
+              this.scene.start("${action?.transition?.to}");
+            }
+          });
+        }
+      `;
+      }
+
+      function generateActions(name, eventType, eventTarget, actions) {
+        let content = '';
+        if (eventType === 'collide') {
+          actions.forEach(({ actionType, action }) => {
+            content += `
+              this.physics.add.overlap(this.${name}, this.${eventTarget}, () => {
+                ${
+                  actionType === 'mainNPCDialog'
+                    ? generateMainNPCDialogAction(action)
+                    : ''
+                }
+                ${
+                  actionType === 'sceneTransition'
+                    ? generateSceneTransition(action)
+                    : ''
+                }
+              });
+            `;
+          });
+        } else {
+          actions.forEach(({ actionType, action }) => {
+            content += `
+              this.${name}.on("${eventType.toLowerCase()}", function () {
+                ${
+                  actionType === 'sceneTransition'
+                    ? generateSceneTransition(action)
+                    : ''
+                }
+                ${
+                  actionType === 'mainNPCDialog'
+                    ? generateMainNPCDialogAction(action)
+                    : ''
+                }
+              }, this);
+            `;
+          });
+        }
+        return content;
+      }
+      if (actionableItem?.label) {
+        createCode += `
+          this.add.text(
+            ${actionableItem.position.x},
+            ${actionableItem.position.y - actionableItem.size.height},
+            "${actionableItem.label}",
+            ${JSON.stringify(sceneConfig?.labelStyles)}
+          ).setOrigin(0.5);
+        `;
+      }
+
+      if (actionableItem?.events && actionableItem?.events.length > 0) {
+        actionableItem?.events.forEach(
+          ({ eventType, actions, eventTarget }) => {
+            createCode += generateActions(
+              actionableItem?.name,
+              eventType,
+              eventTarget,
+              actions
+            );
+          }
+        );
+      }
+    });
+  }
   return createCode;
 }
 
