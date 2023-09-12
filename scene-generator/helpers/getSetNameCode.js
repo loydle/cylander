070ab678be97875b62c22135e96a69574ededb4d
Matthieu van den Biggelaar <vdbiggelaar@gmail.com>
2023-09-12 23:50:07 +0200

    test: update test cases

--- scene-generator/helpers/getSetNameCode.js
+++ scene-generator/helpers/getSetNameCode.js
@@ -1,6 +1,6 @@
 function getSetNameCode(name) {
- if (!name) throw Error('Actionable Item name is missing.');
- return `this.${name}.name = "${name}";`;
+  if (!name) throw Error('Actionable Item name is missing.');
+  return `this.${name}.name = "${name}";`;
 }
 
 module.exports = getSetNameCode;
