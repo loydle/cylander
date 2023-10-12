e9c83a96f68099afbe082b5f7f81936a0d815443
Cleo Santos <cleo.santos@ing.com>
2023-10-12 11:30:24 +0200

    Sprite image frame rate workaround

--- scene-generator/helpers/getActionableItemSpriteCode.js
+++ scene-generator/helpers/getActionableItemSpriteCode.js
@@ -6,6 +6,7 @@ function getActionableItemSpriteCode(actionableItem) {
   });
   const ${actionableItem?.name}Sprite = this.add.sprite(${actionableItem?.position.x},${actionableItem?.position.y}, '${actionableItem?.name}').setScale(${actionableItem?.scale});
   ${actionableItem?.name}Sprite.play({ key: '${actionableItem?.key}', repeat: ${actionableItem?.repeat} });
+  ${actionableItem?.name}Sprite.anims.msPerFrame = Math.floor(${actionableItem?.duration}/(${actionableItem?.frameRate}*${actionableItem?.repeat}));
     
   this.tweens.add({
       targets: ${actionableItem?.name}Sprite,
